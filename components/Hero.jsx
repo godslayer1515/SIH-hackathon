7c7f6470aba53dce3653be4581811b34cd256172
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-09-23 01:17:48 +0530

    added few more files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
             <h2>Collaboration with Ministry:</h2>
             <p>Our project is in collaboration with Minstry of Law and Justice, aligning with their vision to improve legal services accessibility in India.</p>
             <h2>Deliverables for SIH 2023:</h2>
-            <p>As part of the Smart India Hackathon 2023, we are presenting a functional prototype of our eMarketplace platform, including wireframes, user stories, and a video demonstration showcasing the platform's key features.</p>
+            <p>As part of the Smart India Hackathon 2023, we are presenting a functional prototype of our eMarketplace platform, including wireframes, user stories, and a video demonstration showcasing the platform{'\''}s key features.</p>
             <h2>Future Plans:</h2>
             <p>Beyond the hackathon, we are committed to refining and expanding our platform, aiming to make a lasting impact on the legal services sector in India.</p>
         </div>
