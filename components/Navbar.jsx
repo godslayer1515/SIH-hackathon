c7bad45c4f1b7577848fe01556c8444c50db07ba
Siddharth shukla <siddharthshukla122333@gmail.com>
2023-09-24 21:44:43 +0530

    mobile view navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,25 +31,30 @@ const Navbar = () => {
                         </a>
                     ))}
                 </nav>
-                <nav className={`md:ml-auto md:mr-auto flex flex-col md:flex-row items-center text-base justify-center md:hidden ${Toggle ? 'hidden' : ''}`}>
-                    {headerlinks.map((link) => (
-                        <a
-                            key={link.id}
-                            href={`#${link.name}`}
-                            className="mr-5 cursor-pointer hover:text-white hover:text-bold text-black-500 py-[2px]"
-                        >
-                            {link.name.toUpperCase()}
-                        </a>
-                    ))}
-                </nav>
+                
                 <button className="inline-flex items-center bg-white-100 border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 md:hidden" onClick={onToggle}>
                     {Toggle ? (
-                        <Image src={menu} width={45} height={30} alt="menu" className='rounded-[20px] hover:drop-shadow-xl' />
+                        <Image src={menu} width={45} height={30} alt="menu" className='rounded-[20px] hover:drop-shadow-xl ml-[20vh]' />
                     ) : (
                         <Image src={menuclose} width={30} height={20} alt="cross" />
                     )}
                 </button>
-            </div>
+                </div>
+                <div className='flex justify-start items-center'>
+                    <nav className={`md:ml-auto md:mr-auto flex flex-col  items-start text-base justify-center md:hidden fixed mt-[403px] ml-[2vw] ${Toggle ? 'hidden' : 'z-[50]'} mt-[30vh] bg-gradient-to-b from-[#97e5ec] to-[#3a757c] rounded-[10px]`}>
+                        {headerlinks.map((link) => (
+                            <a
+                                key={link.id}
+                                href={`#${link.name}`}
+                                className="mr-5 ml-[1vw] cursor-pointer hover:text-white hover:text-bold text-black-500 py-[2px]"
+                            >
+                                {link.name.toUpperCase()}
+                            </a>
+                        ))}
+                    </nav>
+                </div>
+                
+            
         </nav>
     );
 };
